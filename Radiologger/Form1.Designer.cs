4f7d58a2bceb384c8187bd94cc4e36c5c0dda237
Dmytro <dku4it@gmail.com>
2025-10-28 13:29:05 +0200

    Add files via upload

---
+++ Radiologger/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace _03Radiograbber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button11 = new Button();
+            button12 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label2 = new Label();
+            comboBox11 = new ComboBox();
+            progressBar11 = new ProgressBar();
+            pictureBox1 = new PictureBox();
+            comboBox12 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            jrock = new PictureBox();
+            svobodnoe = new PictureBox();
+            textBox15 = new TextBox();
+            toolStripStatusLabel12 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            toolStripStatusLabel5 = new ToolStripStatusLabel();
+            statusStrip1 = new StatusStrip();
+            comboBox7 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)jrock).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)svobodnoe).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button11
+            // 
+            button11.Location = new Point(419, 119);
+            button11.Name = "button11";
+            button11.Size = new Size(53, 23);
+            button11.TabIndex = 12;
+            button11.Text = "Select";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click_1;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.LightGray;
+            button12.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button12.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button12.ForeColor = SystemColors.ControlText;
+            button12.Location = new Point(397, 196);
+            button12.Name = "button12";
+            button12.Size = new Size(78, 35);
+            button12.TabIndex = 2;
+            button12.Text = "Run";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = SystemColors.GrayText;
+            label3.Location = new Point(21, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(139, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Select destination Folder:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = SystemColors.GrayText;
+            label4.Location = new Point(21, 205);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Enter URL";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = SystemColors.GrayText;
+            label2.Location = new Point(35, 243);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Status:";
+            // 
+            // comboBox11
+            // 
+            comboBox11.FormattingEnabled = true;
+            comboBox11.Location = new Point(85, 202);
+            comboBox11.Name = "comboBox11";
+            comboBox11.Size = new Size(306, 23);
+            comboBox11.TabIndex = 1;
+            comboBox11.Tag = "";
+            comboBox11.SelectedIndexChanged += comboBox11_SelectedIndexChanged;
+            // 
+            // progressBar11
+            // 
+            progressBar11.Location = new Point(0, 276);
+            progressBar11.Name = "progressBar11";
+            progressBar11.Size = new Size(487, 14);
+            progressBar11.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(487, 105);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // comboBox12
+            // 
+            comboBox12.FormattingEnabled = true;
+            comboBox12.Location = new Point(165, 159);
+            comboBox12.Name = "comboBox12";
+            comboBox12.Size = new Size(171, 23);
+            comboBox12.TabIndex = 13;
+            comboBox12.TextChanged += ComboBox12_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = SystemColors.GrayText;
+            label1.Location = new Point(97, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Select File:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(343, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(53, 23);
+            button1.TabIndex = 14;
+            button1.Text = "Go to";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // jrock
+            // 
+            jrock.BackColor = SystemColors.Control;
+            jrock.Image = (Image)resources.GetObject("jrock.Image");
+            jrock.Location = new Point(46, 150);
+            jrock.Name = "jrock";
+            jrock.Size = new Size(40, 40);
+            jrock.SizeMode = PictureBoxSizeMode.CenterImage;
+            jrock.TabIndex = 14;
+            jrock.TabStop = false;
+            jrock.Visible = false;
+            // 
+            // svobodnoe
+            // 
+            svobodnoe.BackColor = SystemColors.Control;
+            svobodnoe.BackgroundImageLayout = ImageLayout.Zoom;
+            svobodnoe.Image = (Image)resources.GetObject("svobodnoe.Image");
+            svobodnoe.Location = new Point(46, 150);
+            svobodnoe.Name = "svobodnoe";
+            svobodnoe.Size = new Size(40, 40);
+            svobodnoe.SizeMode = PictureBoxSizeMode.CenterImage;
+            svobodnoe.TabIndex = 15;
+            svobodnoe.TabStop = false;
+            svobodnoe.Tag = "";
+            svobodnoe.Visible = false;
+            // 
+            // textBox15
+            // 
+            textBox15.BackColor = SystemColors.ButtonFace;
+            textBox15.BorderStyle = BorderStyle.None;
+            textBox15.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox15.ForeColor = Color.FromArgb(0, 64, 0);
+            textBox15.Location = new Point(83, 243);
+            textBox15.Multiline = true;
+            textBox15.Name = "textBox15";
+            textBox15.ReadOnly = true;
+            textBox15.Size = new Size(392, 34);
+            textBox15.TabIndex = 0;
+            textBox15.TabStop = false;
+            textBox15.MouseDoubleClick += textBox15_MouseDoubleClick;
+            // 
+            // toolStripStatusLabel12
+            // 
+            toolStripStatusLabel12.AutoSize = false;
+            toolStripStatusLabel12.BorderSides = ToolStripStatusLabelBorderSides.Right;
+            toolStripStatusLabel12.BorderStyle = Border3DStyle.SunkenInner;
+            toolStripStatusLabel12.ForeColor = SystemColors.GrayText;
+            toolStripStatusLabel12.Name = "toolStripStatusLabel12";
+            toolStripStatusLabel12.Size = new Size(175, 19);
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.AutoSize = false;
+            toolStripStatusLabel2.BorderSides = ToolStripStatusLabelBorderSides.Right;
+            toolStripStatusLabel2.BorderStyle = Border3DStyle.SunkenInner;
+            toolStripStatusLabel2.ForeColor = Color.Gray;
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.RightToLeft = RightToLeft.No;
+            toolStripStatusLabel2.Size = new Size(162, 19);
+            toolStripStatusLabel2.Spring = true;
+            // 
+            // toolStripStatusLabel5
+            // 
+            toolStripStatusLabel5.AutoSize = false;
+            toolStripStatusLabel5.ForeColor = Color.Gray;
+            toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            toolStripStatusLabel5.Size = new Size(135, 19);
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.BackColor = Color.Transparent;
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel12, toolStripStatusLabel2, toolStripStatusLabel5 });
+            statusStrip1.Location = new Point(0, 287);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(487, 24);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 9;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // comboBox7
+            // 
+            comboBox7.FormattingEnabled = true;
+            comboBox7.Location = new Point(166, 119);
+            comboBox7.Name = "comboBox7";
+            comboBox7.Size = new Size(247, 23);
+            comboBox7.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(487, 311);
+            Controls.Add(comboBox7);
+            Controls.Add(progressBar11);
+            Controls.Add(textBox15);
+            Controls.Add(svobodnoe);
+            Controls.Add(jrock);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(comboBox12);
+            Controls.Add(pictureBox1);
+            Controls.Add(statusStrip1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button12);
+            Controls.Add(comboBox11);
+            Controls.Add(button11);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(503, 350);
+            MinimumSize = new Size(503, 350);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Radio Logger 2.4.6";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)jrock).EndInit();
+            ((System.ComponentModel.ISupportInitialize)svobodnoe).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button11;
+        private Button button12;
+        private Label label3;
+        private Label label4;
+        private Label label2;
+        private ComboBox comboBox11;
+        private ProgressBar progressBar11;
+        private PictureBox pictureBox1;
+        private ComboBox comboBox12;
+        private Label label1;
+        private Button button1;
+        private PictureBox jrock;
+        private PictureBox svobodnoe;
+        private TextBox textBox15;
+        private ToolStripStatusLabel toolStripStatusLabel12;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripStatusLabel toolStripStatusLabel5;
+        private StatusStrip statusStrip1;
+        private ComboBox comboBox7;
+    }
+}
---
+++ Radiologger/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace _03Radiograbber
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+        private CancellationTokenSource radioCts;
+        private string lastTrack = "";
+        DateTime delay1 = DateTime.Now;
+        int count1 = 0;
+
+
+        // словарь соответствий URL → файл
+        private Dictionary<string, string> urlToFile = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            // добавляем путь Загрузок Windows в комбобокс
+            comboBox7.Items.Add(GetDownloadsPath());
+
+        }
+
+
+        private string FormatSessionTime(TimeSpan ts)
+        {
+            return $"{(int)ts.TotalHours}:{ts.Minutes:D2}";
+        }
+
+
+
+
+
+
+
+        // ищем путь Загрузок Windows чтобы потом добавить в комбобокс
+       
+    [DllImport("shell32.dll")]
+    private static extern int SHGetKnownFolderPath(
+    [MarshalAs(UnmanagedType.LPStruct)] Guid rfid,
+    uint dwFlags,
+    IntPtr hToken,
+    out IntPtr ppszPath);
+
+    private static readonly Guid FolderDownloads =
+        new Guid("374DE290-123F-4565-9164-39C4925E467B");
+
+    public static string GetDownloadsPath()
+    {
+        SHGetKnownFolderPath(FolderDownloads, 0, IntPtr.Zero, out var outPath);
+        string path = Marshal.PtrToStringUni(outPath);
+        Marshal.FreeCoTaskMem(outPath);
+        return path;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private async Task MonitorRadioStream(string url, string saveFolder, ProgressBar progressBar, Control statusControl, CancellationToken token)
+        {
+            DateTime startTime = DateTime.Now;
+            string filePath = Path.Combine(saveFolder, comboBox12.Text);
+            await File.AppendAllTextAsync(filePath, $"-------  {DateTime.Now.ToString("dd.MM.yyyy HH:mm")}  ({comboBox11.Text}) {Environment.NewLine}");
+
+            progressBar.Style = ProgressBarStyle.Marquee;
+            comboBox11.Enabled = false;
+            comboBox7.Enabled = false;
+            button11.Enabled = false;
+            comboBox12.Enabled = false;
+
+            string lastTitle = "";
+
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    using var client = new HttpClient();
+                    client.DefaultRequestHeaders.Add("Icy-MetaData", "1");
+                    using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+                    response.EnsureSuccessStatusCode();
+
+
+                    if (!response.Headers.TryGetValues("icy-metaint", out var metaintValues))
+                    {
+                        statusControl.Invoke(() => statusControl.Text = "Absent icy-metaint");
+                    }
+                    else
+                    {
+                        int metaInt = int.Parse(metaintValues.First());
+                        using var stream = await response.Content.ReadAsStreamAsync(token);
+
+                        // пропускаем блок аудио
+                        byte[] audioBuffer = new byte[metaInt];
+                        int totalRead = 0;
+                        while (totalRead < metaInt)
+                        {
+                            int read = await stream.ReadAsync(audioBuffer, totalRead, metaInt - totalRead, token);
+                            if (read <= 0) throw new Exception("Stream is closed");
+                            totalRead += read;
+                        }
+
+                        // читаем метаданные
+                        int metaLength = stream.ReadByte();
+                        if (metaLength > 0)
+                        {
+                            int metaDataBytes = metaLength * 16;
+                            byte[] metaBuffer = new byte[metaDataBytes];
+                            int metaRead = 0;
+                            while (metaRead < metaDataBytes)
+                            {
+                                int read = await stream.ReadAsync(metaBuffer, metaRead, metaDataBytes - metaRead, token);
+                                if (read <= 0) throw new Exception("Error reading Metadata");
+                                metaRead += read;
+                            }
+
+                            string metaString = Encoding.UTF8.GetString(metaBuffer);
+                            var match = Regex.Match(metaString, "StreamTitle='(.*?)';");
+                            if (match.Success)
+                            {
+                                string currentTitle = match.Groups[1].Value.Trim();
+
+                                if (!string.IsNullOrEmpty(currentTitle) && currentTitle != lastTitle)
+                                {
+                                    lastTitle = currentTitle;
+                                    await File.AppendAllTextAsync(filePath, currentTitle + Environment.NewLine);
+                                    statusControl.Invoke(() => statusControl.Text = currentTitle);
+
+                                    toolStripStatusLabel2.Text = "Last update: " + DateTime.Now.ToString("HH:mm");
+                                    delay1 = DateTime.Now;
+                                    toolStripStatusLabel2.ForeColor = Color.Gray; // меняем цвет текста на стандартный
+                                    count1 = count1 + 1;
+                                    toolStripStatusLabel5.Text = $"Titles saved: {count1}";
+
+                                    this.Invoke(() =>
+                                    {
+                                        this.SelectNextControl(textBox15, true, true, true, true); // переаодим фокус обратно на кнопку
+                                    });
+
+                                }
+                            }
+                        }
+                    }
+
+                    // обновляем время сессии каждую минуту
+                    TimeSpan sessionTime = DateTime.Now - startTime;
+                    toolStripStatusLabel12.Text = "Session time: " + FormatSessionTime(sessionTime);
+
+                    // ждем минуту
+                    await Task.Delay(TimeSpan.FromMinutes(1), token);
+                }
+                catch (TaskCanceledException)
+                {
+                    statusControl.Invoke(() => statusControl.Text = "Stopped");
+                    progressBar.Style = ProgressBarStyle.Blocks;
+                    comboBox11.Enabled = true;
+                    comboBox7.Enabled = true;
+                    button11.Enabled = true;
+                    comboBox12.Enabled = true;
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    statusControl.Invoke(() => statusControl.Text = "Error: " + ex.Message);
+                    await Task.Delay(TimeSpan.FromMinutes(1), token);
+                }
+
+                if (DateTime.Now - delay1 > TimeSpan.FromMinutes(20))
+                {
+                    toolStripStatusLabel2.ForeColor = Color.Red; // меняем цвет текста если 20 мин нет нового тега
+                }
+
+            }
+        }
+
+
+        private async void button11_Click_1(object sender, EventArgs e)
+        {
+
+            using var folderDialog = new FolderBrowserDialog
+            {
+                Description = "Select the Folder for saving"
+            };
+
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                comboBox7.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private async void button12_Click_1(object sender, EventArgs e)
+        {
+            if (radioCts == null)
+            {
+                if (string.IsNullOrWhiteSpace(comboBox11.Text))
+                {
+                    MessageBox.Show("Enter URL to radio stream");
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(comboBox7.Text))
+                {
+                    MessageBox.Show("Enter the Folder for saving");
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(comboBox12.Text))
+                {
+                    MessageBox.Show("Enter File Name to saving");
+                    return;
+                }
+
+
+
+                button12.Text = "Stop";
+                radioCts = new CancellationTokenSource();
+                await MonitorRadioStream(comboBox11.Text, comboBox7.Text, progressBar11, textBox15, radioCts.Token);
+            }
+            else
+            {
+                radioCts.Cancel();
+                radioCts = null;
+                button12.Text = "Run";
+            }
+
+
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string folderPath = comboBox7.Text;
+            string fileName = comboBox12.Text;
+            string filePath = Path.Combine(folderPath, fileName);
+
+            if (File.Exists(filePath))
+            {
+                // открываем папку и выделяем файл
+                System.Diagnostics.Process.Start("explorer.exe", $"/select,\"{filePath}\"");
+            }
+            else if (Directory.Exists(folderPath))
+            {
+                // если файла нет — просто открываем папку
+                System.Diagnostics.Process.Start("explorer.exe", folderPath);
+            }
+            else
+            {
+                MessageBox.Show("Directory not found");
+            }
+        }
+
+        // теперь только URL → файл
+        private void comboBox11_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string url = comboBox11.Text;
+            if (urlToFile.ContainsKey(url))
+            {
+                comboBox12.Text = urlToFile[url];
+            }
+        }
+
+        private void ComboBox12_TextChanged(object sender, EventArgs e)
+        {
+            string iis = comboBox12.Text;
+            switch (iis)
+            {
+                case "Radio_log_Svobodnoe.txt":
+                    jrock.Visible = false;
+                    svobodnoe.Visible = true;
+                    break;
+
+                case "Radio_log_JRock.txt":
+                    jrock.Visible = true;
+                    svobodnoe.Visible = false;
+                    break;
+
+                default:
+                    jrock.Visible = false;
+                    svobodnoe.Visible = false;
+                    break;
+            }
+        }
+
+        // загрузка Setup.ini
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string iniPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.ini");
+
+            if (!File.Exists(iniPath))
+            {
+                MessageBox.Show("Settings.ini not found");
+                return;
+            }
+
+            string currentSection = "";
+            foreach (var rawLine in File.ReadAllLines(iniPath))
+            {
+                string trimmed = rawLine.Trim();
+
+                // пропускаем пустые строки и комментарии (#, ;, //)
+                if (string.IsNullOrWhiteSpace(trimmed) ||
+                    trimmed.StartsWith("#") ||
+                    trimmed.StartsWith(";") ||
+                    trimmed.StartsWith("//"))
+                    continue;
+
+                // секция [ ... ]
+                if (trimmed.StartsWith("[") && trimmed.EndsWith("]"))
+                {
+                    currentSection = trimmed.Trim('[', ']');
+
+                    // если это файл .txt → добавляем в список файлов
+                    if (currentSection.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (!comboBox12.Items.Contains(currentSection))
+                            comboBox12.Items.Add(currentSection);
+                    }
+                    continue;
+                }
+
+                // обработка параметров внутри секций
+                if (currentSection.Equals("destination", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (trimmed.StartsWith("path=", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // добавляем путь из Setup.ini в комбобокс текстом
+                        comboBox7.Text = trimmed.Substring("path=".Length).Trim();
+                        // добавляем путь из Setup.ini в список комбобокса
+                        comboBox7.Items.Add(trimmed.Substring("path=".Length).Trim());
+                    }
+                }
+                else if (currentSection.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (trimmed.StartsWith("url=", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string url = trimmed.Substring("url=".Length).Trim();
+
+                        if (!comboBox11.Items.Contains(url))
+                            comboBox11.Items.Add(url);
+
+                        // сохраняем связь URL → файл
+                        urlToFile[url] = currentSection;
+                    }
+                }
+            }
+
+            // выбираем первый элемент только один раз, если он есть
+            if (comboBox12.Items.Count > 0)
+                comboBox12.SelectedIndex = 0;
+        }
+
+        private void textBox15_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox15.SelectAll();
+        }
+
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
